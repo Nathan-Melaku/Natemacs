ee2a5e198674a854083043b29a7d9617759364f7
Nathan Abeje <nathanmelaku@protonmail.com>
2023-03-06 22:37:43 +0100

    Added emacs config

---
+++ init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(emojify dired-hide-dotfiles dired-single multiple-cursors restclient dired-git-info diredfl dired svelte-mode lsp-java editorconfig prettier-js lsp-ui lsp-mode magit company web-mode json-mode expand-region which-key exec-path-from-shell diredplus diredp dired+ all-the-icons-dired marginalia doom-modeline vertico projectile lua-mode eglot vterm all-the-icons dirvish pdf-tools typescript-mode yasnippet org-tree-slide visual-fill-column org-bullets simple-modeline doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
