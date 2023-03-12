77f73b53122712fe7f2713e7e33c4a268aa7b425
Nathan Abeje <nathanmelaku@protonmail.com>
2023-03-12 20:06:08 +0100

    Added packages.

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(emojify dired-hide-dotfiles dired-single multiple-cursors restclient dired-git-info diredfl dired svelte-mode lsp-java editorconfig prettier-js lsp-ui lsp-mode magit company web-mode json-mode expand-region which-key exec-path-from-shell diredplus diredp dired+ all-the-icons-dired marginalia doom-modeline vertico projectile lua-mode eglot vterm all-the-icons dirvish pdf-tools typescript-mode yasnippet org-tree-slide visual-fill-column org-bullets simple-modeline doom-themes use-package)))
+   '(docker beacon origami devdocs-browser consult toc-org nov-xwidget cursory nov emojify dired-hide-dotfiles dired-single multiple-cursors restclient dired-git-info diredfl dired svelte-mode lsp-java editorconfig prettier-js lsp-ui lsp-mode magit company web-mode json-mode expand-region which-key exec-path-from-shell diredplus diredp dired+ all-the-icons-dired marginalia doom-modeline vertico projectile lua-mode eglot vterm all-the-icons dirvish pdf-tools typescript-mode yasnippet org-tree-slide visual-fill-column org-bullets simple-modeline doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
